89086fc7dfcc37989dd13a09ad16596a2c950840
WilkinsonWorkshop <workshop.minecraftd@gmail.com>
2023-09-30 22:23:12 -0500

    Update tokens.py - Simplify token2id mapping situation - Implement parsing tokens to specific token types

--- proto/tokens.py
+++ proto/tokens.py
@@ -1,147 +1,193 @@
 import enum
-import io
 import typing
 
 from .symbols import Symbol, Column, Lineno, SymbolParser
-from .symbols import (
-    symbol_ispunc,
-    symbol_isname,
-    symbol_isnumeric,
-    symbol_isstring)
+from .symbols import symbol_isnumeric, symbol_isstring
 
-TTErrorMapping:    typing.Mapping[bytes, int] = {}
-TTKwdMapping:      typing.Mapping[bytes, int] = {}
-TTNameMapping:     typing.Mapping[bytes, int] = {}
-TTNumericsMapping: typing.Mapping[bytes, int] = {}
-TTOperMapping:     typing.Mapping[bytes, int] = {}
-TTPuncMapping:     typing.Mapping[bytes, int] = {}
-TTStringsMapping:  typing.Mapping[bytes, int] = {}
-TTCTRLMapping:     typing.Mapping[bytes, int] = {}
+TokenType = typing.TypeVar("TokenType")
+TokenTypeMapping: typing.Mapping[bytes, int] = {}
 
 
-def auto_token(symbol: bytes, *, parent: tuple[int, bytes] | None = None): #type: ignore
+def auto(symbol: bytes): #type: ignore
     """
     Maps token IDs to their respective symbol.
     """
 
-    pair = (enum.auto(), symbol)
+    value = enum.auto()
+    TokenTypeMapping[symbol] = value
 
-    if parent:
-        if parent[1] == b"<keyword>":
-            TTKwdMapping[symbol] = pair[0]
-        elif parent[1] == b"<name:type>":
-            TTNameMapping[symbol] = pair[0]
-        elif parent[1] == b"<numeric>":
-            TTNumericsMapping[symbol] = pair[0]
-        elif parent[1] == b"<operation>":
-            TTOperMapping[symbol] = pair[0]
-        elif parent[1] == b"<punctuation>":
-            TTPuncMapping[symbol] = pair[0]
-        elif parent[1] == b"<string>":
-            TTStringsMapping[symbol] = pair[0]
-        elif parent[1] == b"<ctrl_character>":
-            TTCTRLMapping[symbol] = pair[0]
-
-    return pair
+    return value
 
 
-class TokenType(tuple[int, bytes], enum.ReprEnum):
+class TokenType(int, enum.ReprEnum):
     # Most of the 'types' in this enumerator are
     # too specific for this tokenizer to
     # identifiy on it's own. Most likely, we will
     # keep these definitions here for come parse
     # time.
 
-    Error          = auto_token(b"<error>")
-    ErrorUnknown   = auto_token(b"<error:unknown>")
+    Error          = auto(b"<error>")
+    ErrorUnknown   = auto(b"<error:unknown>")
 
-    Kwd            = auto_token(b"<keyword>")
-    KwdBreak       = auto_token(b"break", parent=Kwd)
-    KwdContinue    = auto_token(b"continue", parent=Kwd)
-    KwdClass       = auto_token(b"class", parent=Kwd)
-    KwdDelete      = auto_token(b"delete", parent=Kwd)
-    KwdElse        = auto_token(b"else", parent=Kwd)
-    KwdFor         = auto_token(b"for", parent=Kwd)
-    KwdIf          = auto_token(b"if", parent=Kwd)
-    KwdImport      = auto_token(b"import", parent=Kwd)
-    KwdNew         = auto_token(b"new", parent=Kwd)
-    KwdPanic       = auto_token(b"panic", parent=Kwd)
-    KwdProto       = auto_token(b"proto", parent=Kwd)
-    KwdRaise       = auto_token(b"raise", parent=Kwd)
-    KwdReturn      = auto_token(b"return", parent=Kwd)
-    KwdWhile       = auto_token(b"while", parent=Kwd)
-    KwdWith        = auto_token(b"with", parent=Kwd)
+    Kwd            = auto(b"<keyword>")
+    KwdBreak       = auto(b"break")
+    KwdContinue    = auto(b"continue")
+    KwdClass       = auto(b"class")
+    KwdDelete      = auto(b"delete")
+    KwdElse        = auto(b"else")
+    KwdFor         = auto(b"for")
+    KwdIf          = auto(b"if")
+    KwdImport      = auto(b"import")
+    KwdNew         = auto(b"new")
+    KwdPanic       = auto(b"panic")
+    KwdProto       = auto(b"proto")
+    KwdRaise       = auto(b"raise")
+    KwdReturn      = auto(b"return")
+    KwdWhile       = auto(b"while")
+    KwdWith        = auto(b"with")
 
-    Name           = auto_token(b"<name>")
-    NameOfType     = auto_token(b"<name:type>")
+    Name           = auto(b"<name>")
+    NameOfType     = auto(b"<name:type>")
 
     # '%' is being used to express `a consecutive
     # series of numerical characters`. This means
     # that we can expect an integer-like sequence
     # in the token at that position.
-    NameOfTypeInt  = auto_token(br"int%", parent=NameOfType)
-    NameOfTypeChar = auto_token(b"char", parent=NameOfType)
-    NameOfTypeStr  = auto_token(b"str", parent=NameOfType)
-    NameOfTypeFlt  = auto_token(b"flt", parent=NameOfType)
-    NameOfTypeDbl  = auto_token(b"dbl", parent=NameOfType)
-    NameOfVar      = auto_token(b"<name:variable>")
+    NameOfTypeInt  = auto(br"int%")
+    NameOfTypeChar = auto(b"char")
+    NameOfTypeStr  = auto(b"str")
+    NameOfTypeFlt  = auto(b"flt")
+    NameOfTypeDbl  = auto(b"dbl")
+    NameOfVar      = auto(b"<name:variable>")
 
-    Num            = auto_token(b"<numeric>")
-    NumFlt         = auto_token(br"%.%", parent=Num)
-    NumHex         = auto_token(br"0x%", parent=Num)
-    NumInt         = auto_token(br"%", parent=Num)
-    NumOct         = auto_token(br"0o%", parent=Num)
+    Num            = auto(b"<numeric>")
+    NumBin         = auto(br"0b%")
+    NumFlt         = auto(br"%.%")
+    NumHex         = auto(br"0x%")
+    NumInt         = auto(br"%")
+    NumOct         = auto(br"0o%")
 
-    Oper           = auto_token(b"<operation>")
-    OperAdd        = auto_token(b"+", parent=Oper)
-    OperAddEq      = auto_token(b"+=", parent=Oper)
-    OperAddressOf  = auto_token(b"&", parent=Oper)
-    OperAssign     = auto_token(b"=", parent=Oper)
-    OperBtAnd      = auto_token(b"&", parent=Oper)
-    OperBtOr       = auto_token(b"|", parent=Oper)
-    OperBtXor      = auto_token(b"^", parent=Oper)
-    OperDecrement  = auto_token(b"--", parent=Oper)
-    OperDelete     = auto_token(b"~", parent=Oper)
-    OperDivide     = auto_token(b"/", parent=Oper)
-    OperDivFloor   = auto_token(b"//", parent=Oper)
-    OperDot        = auto_token(b".", parent=Oper)
-    OperIncrement  = auto_token(b"++", parent=Oper)
-    OperLgAnd      = auto_token(b"&&", parent=Oper)
-    OperLgNot      = auto_token(b"!", parent=Oper)
-    OperLgOr       = auto_token(b"||", parent=Oper)
-    OperLgGt       = auto_token(b">", parent=Oper)
-    OperLgGte      = auto_token(b">=", parent=Oper)
-    OperLgLt       = auto_token(b"<", parent=Oper)
-    OperLgLte      = auto_token(b"<=", parent=Oper)
-    OperPointer    = auto_token(b"*", parent=Oper)
-    OperPower      = auto_token(b"**", parent=Oper)
-    OperRemainder  = auto_token(br"%%", parent=Oper)
-    OperMultiply   = auto_token(b"*", parent=Oper)
-    OperStamp      = auto_token(b"@", parent=Oper)
-    OperSubtract   = auto_token(b"-", parent=Oper)
-    OperSubtractEq = auto_token(b"-=", parent=Oper)
+    Oper           = auto(b"<operation>")
+    OperAdd        = auto(b"+")
+    OperAddEq      = auto(b"+=")
+    OperAddressOf  = auto(b"&")
+    OperAssign     = auto(b"=")
+    OperBtAnd      = auto(b"&")
+    OperBtOr       = auto(b"|")
+    OperBtXor      = auto(b"^")
+    OperDecrement  = auto(b"--")
+    OperDelete     = auto(b"~")
+    OperDivide     = auto(b"/")
+    OperDivFloor   = auto(b"//")
+    OperDot        = auto(b".")
+    OperIncrement  = auto(b"++")
+    OperLgAnd      = auto(b"&&")
+    OperLgNot      = auto(b"!")
+    OperLgOr       = auto(b"||")
+    OperLgGt       = auto(b">")
+    OperLgGte      = auto(b">=")
+    OperLgLt       = auto(b"<")
+    OperLgLte      = auto(b"<=")
+    OperPointer    = auto(b"*")
+    OperPower      = auto(b"**")
+    OperRemainder  = auto(br"%%")
+    OperMultiply   = auto(b"*")
+    OperStamp      = auto(b"@")
+    OperSubtract   = auto(b"-")
+    OperSubtractEq = auto(b"-=")
 
-    Punc           = auto_token(b"<punctuation>")
-    PuncColon      = auto_token(b":", parent=Punc)
-    PuncLBrace     = auto_token(b"{", parent=Punc)
-    PuncLBracket   = auto_token(b"[", parent=Punc)
-    PuncLParen     = auto_token(b"(", parent=Punc)
-    PuncRBrace     = auto_token(b"}", parent=Punc)
-    PuncRBracket   = auto_token(b"]", parent=Punc)
-    PuncRParen     = auto_token(b")", parent=Punc)
-    PuncTerminator = auto_token(b";", parent=Punc)
+    Punc           = auto(b"<punctuation>")
+    PuncColon      = auto(b":")
+    PuncLBrace     = auto(b"{")
+    PuncLBracket   = auto(b"[")
+    PuncLParen     = auto(b"(")
+    PuncRBrace     = auto(b"}")
+    PuncRBracket   = auto(b"]")
+    PuncRParen     = auto(b")")
+    PuncTerminator = auto(b";")
 
-    Str            = auto_token(b"<string>")
-    StrSingleBkt   = auto_token(b"`%`", parent=Str)
-    StrSingleDbl   = auto_token(b"\"%\"", parent=Str)
-    StrSingleSgl   = auto_token(b"'%'", parent=Str)
-    StrTripleBkt   = auto_token(b"```%```", parent=Str)
-    StrTripleDbl   = auto_token(b"\"\"\"%\"\"\"", parent=Str)
-    StrTripleSgl   = auto_token(b"'''%'''", parent=Str)
+    Str            = auto(b"<string>")
+    StrSingleBkt   = auto(b"`%`")
+    StrSingleDbl   = auto(b"\"%\"")
+    StrSingleSgl   = auto(b"'%'")
+    StrTripleBkt   = auto(b"```%```")
+    StrTripleDbl   = auto(b"\"\"\"%\"\"\"")
+    StrTripleSgl   = auto(b"'''%'''")
 
-    CTRLChar       = auto_token(b"<ctrl_character>")
-    CTRLCharEOF    = auto_token(b"EOF", parent=CTRLChar)
-    CTRLCharEOL    = auto_token(b"EOL", parent=CTRLChar)
+    CTRLChar       = auto(b"<ctrl_character>")
+    CTRLCharEOF    = auto(b"EOF")
+    CTRLCharEOL    = auto(b"EOL")
+
+
+def tokens_find_errunk(symbol: Symbol):
+    """
+    Dummy function which always returns
+    `TokenType.ErrorUnknown`.
+    """
+
+    return TokenType.ErrorUnknown
+
+
+def tokens_find_gentype(symbol: Symbol):
+    """
+    Identifies the specific `TokenType` of a
+    generic symbol.
+    """
+
+    try:
+        key = TokenTypeMapping[bytes(symbol)]
+    except:
+        return TokenType.Name
+
+    # Key is `enum.auto` type, not int.
+    return TokenType(key.value) #type: ignore
+
+
+def tokens_find_numtype(symbol: Symbol):
+    """
+    Identifies the specific `TokenType` of a
+    numeric symbol.
+    """
+
+    if symbol.count(b"0b") > 0:
+        return TokenType.NumBin
+    if symbol.count(b".") > 0:
+        return TokenType.NumFlt
+    if symbol.count(b"0x") > 0:
+        return TokenType.NumHex
+    if symbol.count(b"0o") > 0:
+        return TokenType.NumOct
+
+    return TokenType.NumInt
+
+
+def tokens_find_strtype(symbol: Symbol):
+    """
+    Identifies the specific `TokenType` of a
+    string symbol.
+    """
+
+    if len(symbol) >= 6 and symbol[:3] == symbol[-3:]:
+        sample = symbol[:3]
+    elif len(symbol) >= 2 and symbol[:1] == symbol[-1:]:
+        sample = symbol[:2]
+    else:
+        return TokenType.ErrorUnknown
+
+    if sample == b"`":
+        return TokenType.StrSingleBkt
+    if sample == b"\"":
+        return TokenType.StrSingleDbl
+    if sample == b"'":
+        return TokenType.StrSingleSgl
+    if sample == b"```":
+        return TokenType.StrTripleBkt
+    if sample == b"\"\"\"":
+        return TokenType.StrTripleDbl
+    if sample == b"'''":
+        return TokenType.StrTripleSgl
+
+    return TokenType.ErrorUnknown
 
 
 class Token:
@@ -165,15 +211,16 @@ class Token:
         self.file   = file
 
         if symbol_isnumeric(symbol):
-            self.set_type(TokenType.Num)
+            type_finder = tokens_find_numtype
         elif symbol_isstring(symbol):
-            self.set_type(TokenType.Str)
-        elif symbol_ispunc(symbol):
-            self.set_type(TokenType.Punc)
-        elif symbol_isname(symbol):
-            self.set_type(TokenType.Name)
+            type_finder = tokens_find_strtype
         else:
-            self.set_type(TokenType.ErrorUnknown)
+            type_finder = tokens_find_gentype
+
+        self.set_type(type_finder(symbol))
+
+    def __str__(self):
+        return self.symbol.decode()
 
     def __repr__(self):
 
@@ -188,7 +235,7 @@ class Token:
             location = f"(lineno: {self.lineno}, col: {self.column})"
 
         return (
-            f"{self.type_.name}{self.__class__.__name__}"
+            f"{self.get_type().name}"
             f"[{self.symbol.decode()!r}]"
             f"@{location}")
 
@@ -214,6 +261,5 @@ class Tokenizer(SymbolParser):
 
 if __name__ == "__main__":
     with open("grammar/control.vxn", "rb") as fd:
-        tk = Tokenizer(fd)
-        while not tk.end():
-            print(tk.next())
+        for tk in Tokenizer(fd):
+            print(repr(tk))
