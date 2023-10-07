0f4d636067941bf01a423258ee184d0dab5e6c03
WilkinsonK <keenanwilkinson@outlook.com>
2023-10-07 14:01:29 -0500

    Implement literals and expression node types

--- proto/nodes.py
+++ proto/nodes.py
@@ -1,5 +1,7 @@
 import typing
 
+from .tokens import Token, TokenType
+
 
 class TreeNode(typing.Protocol):
     """
@@ -21,7 +23,18 @@ class TreeNode(typing.Protocol):
 class ProgramNode(TreeNode):
     """Top most node in an AST."""
 
-    body: tuple[TreeNode, ...]
+    body: list[TreeNode]
+
+    def __init__(self):
+        self.body = []
+
+    def reduce(self) -> typing.Mapping:
+        return {"program": [node.reduce() for node in self.body]}
+
+    def add(self, node: TreeNode):
+        """Adds a node to this program body."""
+
+        self.body.append(node)
 
 
 class StatementNode(TreeNode):
@@ -29,11 +42,105 @@ class StatementNode(TreeNode):
     A phrase composed of multiple nodes.
     """
 
-    kind: str
+    kind: TokenType
+
+    def __init__(self, kind: TokenType) -> None:
+        self.kind = kind
+
+    @classmethod
+    def expand(cls, mapping: typing.Mapping):
+        return cls(**mapping)
 
 
 class ExpressionNode(StatementNode):
     """
     A node which depends on two or more values
     (e.g. "1 + 2" or "x = y").
+    """
+
+
+class BinaryExpressionNode(ExpressionNode):
+    """
+    A node which depends on three child nodes.
+    A left, an operator, and a right.
+    """
+
+    operator: Token
+    left:     ExpressionNode
+    right:    ExpressionNode
+
+    def __init__(
+        self,
+        operator: Token,
+        left: ExpressionNode,
+        right: ExpressionNode):
+
+        super().__init__(operator.ttype)
+        self.operator = operator
+        self.left     = left
+        self.right    = right
+
+    def __repr__(self):
+        info = (
+            "["
+            f"left: {self.left}, op: {self.operator!r}, right: {self.right}"
+            "]"
+        )
+        return f"{self.__class__.__name__}{info}"
+
+    def reduce(self) -> typing.Mapping:
+        return {
+            "kind": self.kind,
+            "operator": self.operator.symbol,
+            "lineno": self.operator.lineno,
+            "column": self.operator.column,
+            "left": self.left.reduce(),
+            "right": self.right.reduce()
+        }
+
+
+class LiteralNode(ExpressionNode):
+    """A literal value."""
+
+    value: Token
+
+    def __init__(self, value: Token) -> None:
+        super().__init__(value.ttype)
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}[value: {self.value!r}]"
+
+    def reduce(self) -> typing.Mapping:
+        return {
+            "kind": self.kind,
+            "value": self.value.symbol,
+            "lineno": self.value.lineno,
+            "column": self.value.column
+        }
+
+    @property
+    def ttype(self):
+        return self.value.ttype
+
+
+class LiteralIdentNode(LiteralNode):
+    """
+    A literal expression which identifies another
+    instance.
+    """
+
+
+class LiteralNumericNode(LiteralNode):
+    """A literal numerical value."""
+
+
+class LiteralFltNode(LiteralNumericNode):
+    """A literal floating number value."""
+
+
+class LiteralIntNode(LiteralNumericNode):
+    """
+    A literal numerical value which digests to an
+    int.
     """
