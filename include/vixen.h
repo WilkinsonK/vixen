b2a9f32bea66bb39e4bfcda272686b2afb2e4788
WilkinsonK <keenanwilkinson@outlook.com>
2023-10-13 10:12:45 -0500

    reoganize source stucture

--- include/vixen.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIXEN
-#include "vixen/objects.h"
-#include "vixen/strings.h"
-#include "vixen/tokens.h"
-// #include "vixen/lexer.h"
-#include "vixen/parser.h"
-#define VIXEN
-#endif
--- include/vixen/parser.h
+++
@@ -1,3 +0,0 @@
-#ifndef VXN_PARSER
-#define VXN_PARSER
-#endif
