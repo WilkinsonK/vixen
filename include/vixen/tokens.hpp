45aabfbc57666ff2d78b280f1cfc181775b85874
WilkinsonK <keenanwilkinson@outlook.com>
2023-10-14 16:37:41 -0500

    Remap tokens name mapping

--- include/vixen/tokens.hpp
+++ include/vixen/tokens.hpp
@@ -100,104 +100,124 @@ namespace vixen::tokens {
         CTRLCharEOL
     };
 
+    #define TYPEMAPPER(NAME, TYPE, REPR) {NAME, {TYPE, REPR}}
     // Unlike the prototype, we are only mapping
     // enumerations that that we can know to be
     // predictable. Such as punctuation, supported
     // operators and keywords.
     // All other token types can be implicitly
     // discerned based on context after parse time.
-    static std::unordered_map<std::string, TokenType> TokenTypeMapping = {
-        {"<keyword>", TokenType::Kwd},
-        {"as", TokenType::KwdAs},
-        {"break", TokenType::KwdBreak},
-        {"catch", TokenType::KwdCatch},
-        {"continue", TokenType::KwdContinue},
-        {"const", TokenType::KwdConstant},
-        {"class", TokenType::KwdClass},
-        {"default", TokenType::KwdDefault},
-        {"delete", TokenType::KwdDelete},
-        {"else", TokenType::KwdElse},
-        {"for", TokenType::KwdFor},
-        {"from", TokenType::KwdFrom},
-        {"func", TokenType::KwdFunc},
-        {"if", TokenType::KwdIf},
-        {"import", TokenType::KwdImport},
-        {"include", TokenType::KwdInclude},
-        {"new", TokenType::KwdNew},
-        {"nil", TokenType::KwdNil},
-        {"null", TokenType::KwdNull},
-        {"panic", TokenType::KwdPanic},
-        {"proto", TokenType::KwdProto},
-        {"raise", TokenType::KwdRaise},
-        {"return", TokenType::KwdReturn},
-        {"static", TokenType::KwdStatic},
-        {"try", TokenType::KwdTry},
-        {"while", TokenType::KwdWhile},
-        {"with", TokenType::KwdWith},
-
-        {"<operation>", TokenType::Oper},
-        {"?", TokenType::OperAsk},
-        {"=", TokenType::OperAssign},
-        {"&", TokenType::OperBtAnd},
-        {"|", TokenType::OperBtOr},
-        {"^", TokenType::OperBtXor},
-        {"--", TokenType::OperDecrement},
-        {"~", TokenType::OperDelete},
-        {"/", TokenType::OperDivide},
-        {"//", TokenType::OperDivFloor},
-        {"++", TokenType::OperIncrement},
-        {"&&", TokenType::OperLgAnd},
-        {"!", TokenType::OperLgNot},
-        {"||", TokenType::OperLgOr},
-        {">", TokenType::OperLgGt},
-        {">=", TokenType::OperLgGte},
-        {"<", TokenType::OperLgLt},
-        {"<=", TokenType::OperLgLte},
-        {"-", TokenType::OperMinus},
-        {"-=", TokenType::OperMinusEq},
-        {"%", TokenType::OperModulus},
-        {"+", TokenType::OperPlus},
-        {"+=", TokenType::OperPlusEq},
-        {"**", TokenType::OperPower},
-        {"->", TokenType::OperPtrAttr},
-        {"@", TokenType::OperStamp},
-        {"*", TokenType::OperStar},
-
-        // You gotta ask yourself one question,
-        // 'Am I feelin' lucky?'
-        {"<punctuation>", TokenType::Punc},
-        {":", TokenType::PuncColon},
-        {",", TokenType::PuncComma},
-        {".", TokenType::PuncDot},
-        {"{", TokenType::PuncLBrace},
-        {"[", TokenType::PuncLBracket},
-        {"(", TokenType::PuncLParen},
-        {"}", TokenType::PuncRBrace},
-        {"]", TokenType::PuncRBracket},
-        {")", TokenType::PuncRParen},
-        {";", TokenType::PuncTerminator},
-
-        {"<string>", TokenType::Str},
-        {"`", TokenType::StrSingleBkt},
-        {"\"", TokenType::StrSingleDbl},
-        {"'", TokenType::StrSingleSgl},
-        {"```", TokenType::StrTripleBkt},
-        {"\"\"\"", TokenType::StrTripleDbl},
-        {"'''", TokenType::StrTripleSgl},
-
-        {"ctrl_character", TokenType::CTRLChar},
-        {"EOF", TokenType::CTRLCharEOF},
-        {"EOL", TokenType::CTRLCharEOL}
+    static std::unordered_map<std::string, std::pair<TokenType, std::string>> TokenTypeMapping = {
+        TYPEMAPPER("<error>", TokenType::Error, "Error"),
+        TYPEMAPPER("<error:unknown>", TokenType::ErrorUnknown, "ErrorUnknown"),
+        TYPEMAPPER("<error:bad_string>", TokenType::ErrorBadString, "ErrorBadString"),
+        TYPEMAPPER("<keyword>", TokenType::Kwd, "Kwd"),
+        TYPEMAPPER("as", TokenType::KwdAs, "KwdAs"),
+        TYPEMAPPER("break", TokenType::KwdBreak, "KwdBreak"),
+        TYPEMAPPER("catch", TokenType::KwdCatch, "KwdCatch"),
+        TYPEMAPPER("continue", TokenType::KwdContinue, "KwdContinue"),
+        TYPEMAPPER("const", TokenType::KwdConstant, "KwdConstant"),
+        TYPEMAPPER("class", TokenType::KwdClass, "KwdClass"),
+        TYPEMAPPER("default", TokenType::KwdDefault, "KwdDefault"),
+        TYPEMAPPER("delete", TokenType::KwdDelete, "KwdDelete"),
+        TYPEMAPPER("else", TokenType::KwdElse, "KwdElse"),
+        TYPEMAPPER("for", TokenType::KwdFor, "KwdFor"),
+        TYPEMAPPER("from", TokenType::KwdFrom, "KwdFrom"),
+        TYPEMAPPER("func", TokenType::KwdFunc, "KwdFunc"),
+        TYPEMAPPER("if", TokenType::KwdIf, "KwdIf"),
+        TYPEMAPPER("import", TokenType::KwdImport, "KwdImport"),
+        TYPEMAPPER("include", TokenType::KwdInclude, "KwdInclude"),
+        TYPEMAPPER("new", TokenType::KwdNew, "KwdNew"),
+        TYPEMAPPER("nil", TokenType::KwdNil, "KwdNil"),
+        TYPEMAPPER("null", TokenType::KwdNull, "KwdNull"),
+        TYPEMAPPER("panic", TokenType::KwdPanic, "KwdPanic"),
+        TYPEMAPPER("proto", TokenType::KwdProto, "KwdProto"),
+        TYPEMAPPER("raise", TokenType::KwdRaise, "KwdRaise"),
+        TYPEMAPPER("return", TokenType::KwdReturn, "KwdReturn"),
+        TYPEMAPPER("static", TokenType::KwdStatic, "KwdStatic"),
+        TYPEMAPPER("try", TokenType::KwdTry, "KwdTry"),
+        TYPEMAPPER("while", TokenType::KwdWhile, "KwdWhile"),
+        TYPEMAPPER("with", TokenType::KwdWith, "KwdWith"),
+        TYPEMAPPER("<name>", TokenType::Name, "Name"),
+        TYPEMAPPER("<name:generic>", TokenType::NameGeneric, "NameGeneric"),
+        TYPEMAPPER("<numeric>", TokenType::Num, "Num"),
+        TYPEMAPPER("0b%", TokenType::NumBin, "NumBin"),
+        TYPEMAPPER("%.%", TokenType::NumFlt, "NumFlt"),
+        TYPEMAPPER("0x%", TokenType::NumHex, "NumHex"),
+        TYPEMAPPER("%", TokenType::OperModulus, "OperModulus"),
+        TYPEMAPPER("0o%", TokenType::NumOct, "NumOct"),
+        TYPEMAPPER("<operation>", TokenType::Oper, "Oper"),
+        TYPEMAPPER("&", TokenType::OperBtAnd, "OperBtAnd"),
+        TYPEMAPPER("?", TokenType::OperAsk, "OperAsk"),
+        TYPEMAPPER("=", TokenType::OperAssign, "OperAssign"),
+        TYPEMAPPER("|", TokenType::OperBtOr, "OperBtOr"),
+        TYPEMAPPER("^", TokenType::OperBtXor, "OperBtXor"),
+        TYPEMAPPER("--", TokenType::OperDecrement, "OperDecrement"),
+        TYPEMAPPER("~", TokenType::OperDelete, "OperDelete"),
+        TYPEMAPPER("/", TokenType::OperDivide, "OperDivide"),
+        TYPEMAPPER("//", TokenType::OperDivFloor, "OperDivFloor"),
+        TYPEMAPPER("++", TokenType::OperIncrement, "OperIncrement"),
+        TYPEMAPPER("&&", TokenType::OperLgAnd, "OperLgAnd"),
+        TYPEMAPPER("!", TokenType::OperLgNot, "OperLgNot"),
+        TYPEMAPPER("||", TokenType::OperLgOr, "OperLgOr"),
+        TYPEMAPPER(">", TokenType::OperLgGt, "OperLgGt"),
+        TYPEMAPPER(">=", TokenType::OperLgGte, "OperLgGte"),
+        TYPEMAPPER("<", TokenType::OperLgLt, "OperLgLt"),
+        TYPEMAPPER("<=", TokenType::OperLgLte, "OperLgLte"),
+        TYPEMAPPER("-", TokenType::OperMinus, "OperMinus"),
+        TYPEMAPPER("-=", TokenType::OperMinusEq, "OperMinusEq"),
+        TYPEMAPPER("+", TokenType::OperPlus, "OperPlus"),
+        TYPEMAPPER("+=", TokenType::OperPlusEq, "OperPlusEq"),
+        TYPEMAPPER("**", TokenType::OperPower, "OperPower"),
+        TYPEMAPPER("->", TokenType::OperPtrAttr, "OperPtrAttr"),
+        TYPEMAPPER("@", TokenType::OperStamp, "OperStamp"),
+        TYPEMAPPER("*", TokenType::OperStar, "OperStar"),
+        TYPEMAPPER("<punctuation>", TokenType::Punc, "Punc"),
+        TYPEMAPPER(":", TokenType::PuncColon, "PuncColon"),
+        TYPEMAPPER(",", TokenType::PuncComma, "PuncComma"),
+        TYPEMAPPER(".", TokenType::PuncDot, "PuncDot"),
+        TYPEMAPPER("{", TokenType::PuncLBrace, "PuncLBrace"),
+        TYPEMAPPER("[", TokenType::PuncLBracket, "PuncLBracket"),
+        TYPEMAPPER("(", TokenType::PuncLParen, "PuncLParen"),
+        TYPEMAPPER("}", TokenType::PuncRBrace, "PuncRBrace"),
+        TYPEMAPPER("]", TokenType::PuncRBracket, "PuncRBracket"),
+        TYPEMAPPER(")", TokenType::PuncRParen, "PuncRParen"),
+        TYPEMAPPER(";", TokenType::PuncTerminator, "PuncTerminator"),
+        TYPEMAPPER("<string>", TokenType::Str, "Str"),
+        TYPEMAPPER("`", TokenType::StrSingleBkt, "StrSingleBkt"),
+        TYPEMAPPER("\"", TokenType::StrSingleDbl, "StrSingleDbl"),
+        TYPEMAPPER("'", TokenType::StrSingleSgl, "StrSingleSgl"),
+        TYPEMAPPER("```", TokenType::StrTripleBkt, "StrTripleBkt"),
+        TYPEMAPPER("\"\"\"", TokenType::StrTripleDbl, "StrTripleDbl"),
+        TYPEMAPPER("'''", TokenType::StrTripleSgl, "StrTripleSgl"),
+        TYPEMAPPER("<string:expression>", TokenType::StrExpression, "StrExpression"),
+        TYPEMAPPER("<ctrl_character>", TokenType::CTRLChar, "CTRLChar"),
+        TYPEMAPPER("EOF", TokenType::CTRLCharEOF, "CTRLCharEOF"),
+        TYPEMAPPER("EOL", TokenType::CTRLCharEOL, "CTRLCharEOL")
     };
 
     TokenType tokens_find_errunk(Symbol _) {
         return TokenType::ErrorUnknown;
     }
 
+    std::string tokens_find_genname(Symbol symbol) {
+        if (!TokenTypeMapping.contains(symbol))
+            return "NameGeneric";
+        return TokenTypeMapping[symbol].second;
+    }
+
+    std::string tokens_find_genname(TokenType type) {
+        for (const auto& [key, value] : TokenTypeMapping) {
+            if (value.first == type)
+                return value.second;
+        }
+        return "ErrorUnknown";
+    }
+
     TokenType tokens_find_gentype(Symbol symbol) {
         if (!TokenTypeMapping.contains(symbol))
             return TokenType::NameGeneric;
-        return TokenTypeMapping[symbol];
+        return TokenTypeMapping[symbol].first;
     }
 
     TokenType tokens_find_numtype(Symbol symbol) {
