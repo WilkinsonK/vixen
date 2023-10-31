1636624246ef753fb349030d812d9a89cb0d67f9
WilkinsonK <keenanwilkinson@outlook.com>
2023-10-31 14:11:02 -0500

    Update wrapper script

--- vixenw.sh
+++ vixenw.sh
@@ -3,7 +3,9 @@
 # This is a convenience script to build, clean and
 # test this project.
 VIXEN_BUILDSYS=`which meson`
-VIXEN_BUILDDIR=build/
+VIXEN_BUILDDIR=build
+VIXEN_DEVELOP_setupFLAGS=--buildtype=debug
+VIXEN_RELEASE_setupFLAGS=--buildtype=release
 
 set -eu
 
@@ -15,9 +17,10 @@ Vixen Build Wrapper
 A convenience script for managing this project.
 
 Available Tasks:
-build Build this project executable, libs & tests.
-clean Remove files generated from build system.
-test  Run all tests."
+build         Build this project executable, libs & tests.
+build_release Build a release version of executable, libs & tests
+clean         Remove files generated from build system.
+test          Run all tests."
 }
 
 echo_performance() {
@@ -26,13 +29,20 @@ echo_performance() {
 
 build() {
     echo_performance build
-    [[ ! -d $VIXEN_BUILDDIR ]] && $VIXEN_BUILDSYS setup $VIXEN_BUILDDIR
+    [[ ! -d $VIXEN_BUILDDIR ]] && $VIXEN_BUILDSYS setup $VIXEN_BUILDDIR $VIXEN_DEVELOP_setupFLAGS
+    $VIXEN_BUILDSYS compile -C $VIXEN_BUILDDIR
+}
+
+build_release() {
+    echo_performance build
+    [[ ! -d ${VIXEN_BUILDDIR}_release ]] && $VIXEN_BUILDSYS setup ${VIXEN_BUILDDIR}_release $VIXEN_RELEASE_setupFLAGS
     $VIXEN_BUILDSYS compile -C $VIXEN_BUILDDIR
 }
 
 clean() {
     echo_performance clean
     [[ -d $VIXEN_BUILDDIR ]] && rm -rf $VIXEN_BUILDDIR
+    [[ -d ${VIXEN_BUILDDIR}_release ]] && rm -rf ${VIXEN_BUILDDIR}_release
     find proto/ -type d -name "__pycache__" | xargs -Ixx rm -r xx
 }
 
