c183f32ae47cd3c4de3e8fe80aa6ff344c7a9fd6
WilkinsonWorkshop <workshop.minecraftd@gmail.com>
2023-10-31 11:43:49 -0500

    add token setups

--- tests/vixen/test_tokens.hpp
+++ tests/vixen/test_tokens.hpp
@@ -1,7 +1,23 @@
+#include <fstream>
+
 #include "include/vixen/tokens.hpp"
 #include "tests/vixen/test_testconf.hpp"
 
 namespace test_vixen::tokens {
     using namespace std;
     using namespace vixen::tokens;
+
+    Lexer setup_lexer() {
+        const std::string file_name("examples/test_symbols.vxn");
+        ifstream file(file_name);
+        assert(file.is_open(), "Could not open test file '{}'", file_name);
+
+        Lexer lexer(file, file_name);
+        file.close();
+        return lexer;
+    }
+
+    Token setup_token() {
+        return Token(20, 6, "this_is_a_token");
+    }
 }
