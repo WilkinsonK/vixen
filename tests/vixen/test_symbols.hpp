5fb8d6d6b6720e99a273d292de9a1a1a9aee2bea
WilkinsonK <keenanwilkinson@outlook.com>
2023-10-31 12:48:59 -0500

    make change to repo

--- tests/vixen/test_symbols.hpp
+++ tests/vixen/test_symbols.hpp
@@ -224,7 +224,6 @@ namespace test_vixen::symbols {
 
     void test_parser_parse_expected() {
         RawParser p = setup_parser();
-
         TRIPLET(Symbol) symbols_expected[] = {
             {5, 1, "sx"},
             {5, 3, ":"},
